e58b810971269a6b2215f4a59d94a23da156b51e
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 18:41:57 -0300

    Listagem dos valores únicos de uma coluna com DataFrame.unique().

--- introducao/leitura_dados.py
+++ introducao/leitura_dados.py
@@ -10,3 +10,5 @@ print("\nVisualização das 5 primeiras linhas do DataFrame\n", df.head())
 print("\nVisualização das 10 primeiras linhas do DataFrame\n", df.head(n=10))
 
 print("\nVisualização das 5 primeiras linhas do DataFrame\n", df.tail())
+
+print("\nListar os valores únicos da coluna Bairro\n", df["bairro"].unique())
