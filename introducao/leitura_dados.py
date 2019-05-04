eed0fc267f1e73e5262614d253d5c02c17e88821
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 18:46:16 -0300

    Contagem e normalização de valores de uma coluna com value_counts().

--- introducao/leitura_dados.py
+++ introducao/leitura_dados.py
@@ -12,3 +12,9 @@ print("\nVisualização das 10 primeiras linhas do DataFrame\n", df.head(n=10))
 print("\nVisualização das 5 primeiras linhas do DataFrame\n", df.tail())
 
 print("\nListar os valores únicos da coluna Bairro\n", df["bairro"].unique())
+
+print("\nContagem dos valores de uma coluna (Bairro)\n",
+      df["bairro"].value_counts())
+
+print("\nNormalização dos valores contados da coluna Bairro\n",
+      df["bairro"].value_counts(normalize=True))
