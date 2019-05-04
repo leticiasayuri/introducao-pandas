063fb4f3f1ffa41cabeb28449fbae3a2e1e10891
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 18:36:29 -0300

    Leitura de arquivo CSV com Pandas.

---
+++ introducao/leitura_dados.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("introducao\dados.csv")
+print('DataFrame criado a partir da leitura de dados.csv\n', df)
+
+print("\nVisualização das 5 primeiras linhas do DataFrame\n", df.head())
+
+print("\nVisualização das 10 primeiras linhas do DataFrame\n", df.head(n=10))
+
+print("\nVisualização das 5 primeiras linhas do DataFrame\n", df.tail())
