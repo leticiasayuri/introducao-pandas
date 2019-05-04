12a5243f7337376e5573346fb22908154b1534d2
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 17:20:46 -0300

    Seleção de linhas de acordo com critérios condicionais e condições de múltiplas colunas.

--- introducao/dataframe.py
+++ introducao/dataframe.py
@@ -17,9 +17,21 @@ print(df["Seminário"])
 print(df.describe())
 
 # Ordenação de DataFrame
-# df = df.sort_values(by="Seminário")
+print(df.sort_values(by="Seminário"))
 
 print(df)
 
 # Seleção de valores pelo index
 print(df.loc[3])
+
+# Seleção de acordo com critérios condicionais
+# Boolean Indexing
+
+# Seleção das linhas em que o valor da coluna Seminário seja acima de 8.0
+print(df[df["Seminário"] > 8.0])
+
+# Condições de múltiplas colunas (deve-se usar operadores bitwise)
+
+# Seleção das linhas em que o valor da coluna Seminário seja acima de 8.0 
+# e o valor da coluna Prova não seja menor que 3
+print(df[(df["Seminário"] > 8.0) & (df["Prova"] > 3)])
