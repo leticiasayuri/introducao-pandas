b1f1a262da8a7b069dc2691e3ed7094d1de72bd1
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 22:09:59 -0300

    Alteração de propriedades para geração de gráficos de dispersao estilizados com as propriedades desejadas.

--- introducao/visualizacao_dados.py
+++ introducao/visualizacao_dados.py
@@ -27,7 +27,7 @@ plt.close()
 # Título para o gráfico
 contagem3 = df["bairro"].value_counts().plot.barh(
     title="Número de apartamentos por bairro")
-plt.savefig("introducao\contagem_3.pdf")    
+plt.savefig("introducao\contagem_3.pdf")
 plt.close()
 
 # Gráfico de Dispersão
@@ -43,4 +43,14 @@ plt.close()
 # Gráfico de pizza
 pizza1 = df["quartos"].value_counts().plot.pie()
 plt.savefig("introducao\pizza_1.pdf")
+plt.close()
+
+# Mais sobre Gráfico de Dispersão
+dispersao3 = df.plot.scatter(x="preco", y="area", s=.5)
+plt.savefig("introducao\dispersao_3.pdf")
+plt.close()
+
+# Uso de uma amostragem de dados para montar o gráfico
+df.sample(frac=.1).plot.scatter(x='preco', y='area')
+plt.savefig("introducao\dispersao_4.pdf")
 plt.close()
