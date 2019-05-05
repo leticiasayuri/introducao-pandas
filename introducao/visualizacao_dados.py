7a624b258d815c86653deb5e17cac49b5440aa5f
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 21:27:27 -0300

    Renomeação de variáveis e histogramas para facilitar entendimento.

--- introducao/visualizacao_dados.py
+++ introducao/visualizacao_dados.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("introducao\dados.csv")
 
-histograma = df["preco"].plot.hist()
-plt.savefig("introducao\histograma.pdf")
+histograma1 = df["preco"].plot.hist()
+plt.savefig("introducao\histograma_1.pdf")
 
-histograma = df["preco"].plot.hist(bins=30, edgecolor="black")
-plt.savefig("introducao\histograma_v2.pdf")
+histograma2 = df["preco"].plot.hist(bins=30, edgecolor="black")
+plt.savefig("introducao\histograma_2.pdf")
