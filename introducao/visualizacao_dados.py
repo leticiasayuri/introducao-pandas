69c32afbcebbaf34439f49dee4842dd1c422a3c5
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 21:37:46 -0300

    Geração de gráficos de barras com informações de contagem.

--- introducao/visualizacao_dados.py
+++ introducao/visualizacao_dados.py
@@ -12,3 +12,13 @@ plt.close()
 histograma2 = df["preco"].plot.hist(bins=30, edgecolor="black")
 plt.savefig("introducao\histograma_2.pdf")
 plt.close()
+
+# Gráfico de barras verticais
+contagem1 = df["bairro"].value_counts().plot.bar()
+plt.savefig("introducao\contagem_1.pdf")
+plt.close()
+
+# Gráfico de barras horizontais
+contagem2 = df["bairro"].value_counts().plot.barh()
+plt.savefig("introducao\contagem_2.pdf")
+plt.close()
