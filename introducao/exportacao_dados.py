db7c60bdd4daac6d07717073cad873b34dae6531
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 22:13:19 -0300

    Exportação de dados de um DataFrame para um arquivo CSV.

---
+++ introducao/exportacao_dados.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.DataFrame({'Aluno': ["Wilfred", "Abbie", "Harry", "Julia", "Carrie"],
+                   'Faltas': [3, 4, 2, 1, 4],
+                   'Prova': [2, 7, 5, 10, 6],
+                   'Seminário': [8.5, 7.5, 9.0, 7.5, 8.0]})
+
+df.to_csv("introducao\\aulas.csv")
+
+aulas = pd.read_csv("introducao\\aulas.csv")
+print(aulas)
