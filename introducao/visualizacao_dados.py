2363d4aceef6db1a836dbd09bd4c1a96a8b6a86c
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 22:02:56 -0300

    Geração de gráficos de dispersão.

--- introducao/visualizacao_dados.py
+++ introducao/visualizacao_dados.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("introducao\dados.csv")
 
+# Histograma
 histograma1 = df["preco"].plot.hist()
 plt.savefig("introducao\histograma_1.pdf")
 plt.close()
@@ -27,4 +28,14 @@ plt.close()
 contagem3 = df["bairro"].value_counts().plot.barh(
     title="Número de apartamentos por bairro")
 plt.savefig("introducao\contagem_3.pdf")    
+plt.close()
+
+# Gráfico de Dispersão
+dispersao1 = df.plot.scatter(x="preco", y="area")
+plt.savefig("introducao\dispersao_1.pdf")
+plt.close()
+
+plt.style.use("ggplot")
+dispersao2 = df.plot.scatter(x="preco", y="area")
+plt.savefig("introducao\dispersao_2.pdf")
 plt.close()
