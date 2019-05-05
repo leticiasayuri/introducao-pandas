5043361a19b7703967cb9b52f0ede1b830967be9
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 21:34:03 -0300

    Fechamento do recurso disponibilizado por plt para que os gráficos não sejam sobrepostos num mesmo PDF quando exportado mais de um gráfico.

--- introducao/visualizacao_dados.py
+++ introducao/visualizacao_dados.py
@@ -7,6 +7,8 @@ df = pd.read_csv("introducao\dados.csv")
 
 histograma1 = df["preco"].plot.hist()
 plt.savefig("introducao\histograma_1.pdf")
+plt.close()
 
 histograma2 = df["preco"].plot.hist(bins=30, edgecolor="black")
 plt.savefig("introducao\histograma_2.pdf")
+plt.close()
