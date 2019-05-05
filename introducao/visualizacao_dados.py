ab9fd5dc926d0e076a2e556778e3a53b5f5b39e9
Leticia Sayuri <leticiasyrk@gmail.com>
2019-05-04 21:25:12 -0300

    Geração de outro histrograma, especificando a quantidade de bins e a cor das bordas.

--- introducao/visualizacao_dados.py
+++ introducao/visualizacao_dados.py
@@ -7,3 +7,6 @@ df = pd.read_csv("introducao\dados.csv")
 
 histograma = df["preco"].plot.hist()
 plt.savefig("introducao\histograma.pdf")
+
+histograma = df["preco"].plot.hist(bins=30, edgecolor="black")
+plt.savefig("introducao\histograma_v2.pdf")
